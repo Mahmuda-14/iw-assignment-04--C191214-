d7f7fe01d4843d01e0d620fe35069f341cbbb2cf
Mahmuda-14 <mahmudaiiuc14@gmail.com>
2024-01-07 17:58:00 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const _ = require('underscore');
+
+const array = ["Java", "Javascript", "Typescript"];
+
+const arr1 = _.contains(array, "Java"); 
+
+console.log(arr1);
+
+
+const arr2 = _.contains(array, "C++");  
+
+console.log(arr2);
